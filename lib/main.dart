e67c5d0beccba257838da24e72057e42ea048eb9
Fernando Candiani <candianirfernando@gmail.com>
2022-11-14 09:11:14 -0300

    Created Button

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add)
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -35,6 +41,11 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
